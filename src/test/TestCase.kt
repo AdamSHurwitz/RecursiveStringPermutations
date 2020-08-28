fded56765a035d0fec312e8db461b5584ddbb508
Adam Hurwitz <3938076+AdamSHurwitz@users.noreply.github.com>
2020-08-27 20:16:17 -0700

    feature: Add JUnit 5 Parameterized Test

---
+++ src/test/TestCase.kt
@@ -0,0 +1,6 @@
+package test
+
+data class TestCase(
+        val input: String,
+        val expect: String
+)
--- src/test/TestSolve.kt
+++ src/test/TestSolve.kt
@@ -7,10 +7,27 @@ import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestMethodOrder
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TestSolve {
+
+    private fun testCases() = Stream.of(
+            TestCase("inputOne", "inputOne"),
+            TestCase("inputTwo", "inputTwo"),
+            TestCase("inputThree", "inputThree")
+    )
+
+    @ParameterizedTest
+    @MethodSource("testCases")
+    fun `TestParam`(test: TestCase) {
+        printAssert(test.input, test.expect)
+        assertThat(test.input).isEqualTo(test.expect)
+    }
+
     @Test
     @Order(1)
     fun `First test`() {
