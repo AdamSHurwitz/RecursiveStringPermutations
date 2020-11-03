b7c6abc71515c48b0b3ff718e4e38f1744f5c9d1
Adam Hurwitz <3938076+AdamSHurwitz@users.noreply.github.com>
2020-11-02 17:16:14 -0800

    docs: Space complexity and optimization notes

--- src/Solve.kt
+++ src/Solve.kt
@@ -2,6 +2,10 @@
  * 1. Iterate through each char and move the char to each position of the input String
  * 2. Add each version to the Set
  * Time complexity: O(n^2)
+ * Space complexity: O(n^n)
+ *
+ * See: Generate Unique String Permutations Recursively - Optimization and Complexity
+ * [https://codereview.stackexchange.com/a/251518/220526]
  **/
 fun allPerm(input: String, lookAtIndex: Int, set: HashSet<String>): HashSet<String> {
     if (lookAtIndex <= input.length - 1) {
